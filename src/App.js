fd9309786087efa725fc9997ff91b4b9d328d63a
Piper <pipersu512@gmail.com>
2020-11-15 02:16:00 -0800

    tensorflow, markdown works

--- src/App.js
+++ src/App.js
@@ -8,6 +8,14 @@ function App() {
     window.location.href="http://localhost:"+port;
   }
 
+  function openSasCloud(){
+    window.location.href="https://www.pdesas.org/Profile/Account/Login"
+  }
+
+  function openTableauCloud(){
+    window.location.href="https://www.tableau.com/products/linux"
+  }
+  
 
   return (
     <div>
@@ -15,20 +23,20 @@ function App() {
         <button onClick={() => handleRStudioOnClick(10001)}> RStudio 
         </button>
         <button onClick={() => handleRStudioOnClick(6080)}> Spyder </button>
-        <button > IBM SAS </button>
+        <button onClick={openSasCloud}> IBM SAS </button>
         <button> Git </button>
         <button onClick={() => handleRStudioOnClick(10000)}> Jupyter Notebook </button>
-        <button> Orange </button>
+        <button onClick={() => handleRStudioOnClick(6901)}>  Orange </button>
         <button> Visual Studio Code IDE </button>
       </div>
       
       <div>
           <button> Apache Hadoop </button>
           <button> Apache Spark </button>
-          <button> Tableau </button>
+          <button onClick={openTableauCloud}> Tableau </button>
           <button> SonarQube & SonarScanner </button>
-          <button> TensorFlow </button>
-          <button> Markdown </button>
+          <button onClick={() => handleRStudioOnClick(10003)}> TensorFlow </button>
+          <button onClick={() => handleRStudioOnClick(9090)}> Markdown </button>
       </div>
       
     </div>
