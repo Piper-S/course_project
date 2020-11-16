fcda04f195c27d2bb11a077d84908ea560f49bac
Piper <pipersu512@gmail.com>
2020-11-15 23:23:56 -0800

    Orange & Sonarqube
    finished with the content. Need to update frontend to look better.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import './App.css';
 
 function App() {
+  var windowFeatures = "menubar=yes,toolbar=yes, location=yes,resizable=yes,scrollbars=yes,status=yes";
 
   function handleOnClick(port) {
     // e.preventDefault();
     console.log(port);
-    window.location.href="http://localhost:"+port;
+    window.open("http://localhost:"+port, "_blank");
   }
 
   function openSasCloud(){
-    window.location.href="https://www.pdesas.org/Profile/Account/Login"
+    // window.location.href="https://www.pdesas.org/Profile/Account/Login"
+    window.open("https://www.pdesas.org/Profile/Account/Login", "_blank",windowFeatures)
   }
 
   function openTableauCloud(){
-    window.location.href="https://www.tableau.com/products/linux"
+    window.open("https://www.tableau.com/products/linux", "_blank",windowFeatures)
   }
-  var windowFeatures = "menubar=yes,toolbar=yes, location=yes,resizable=yes,scrollbars=yes,status=yes";
+  
   function openTerminal(port, terminal_port){
     window.open("http://localhost:"+port+"/terminals/"+terminal_port, "_windowname", windowFeatures)
   }
@@ -23,25 +25,24 @@ function App() {
 
   return (
     <div>
-      <div>
-        <button onClick={() => handleOnClick(10001)}> RStudio 
-        </button>
-        <button onClick={() => handleOnClick(6080)}> Spyder </button>
-        <button onClick={openSasCloud}> IBM SAS </button>
-        <button onClick={() => handleOnClick(10000)}>  Git </button>
-        <button onClick={() => handleOnClick(10000)}> Jupyter Notebook </button>
-        <button onClick={() => handleOnClick(6901)}>  Orange </button>
-        <button onClick={() => handleOnClick(8443)}> Visual Studio Code IDE </button>
-      </div>
-      
-      <div>
-          <button onClick={() => openTerminal(9433, 1)}> Apache Hadoop </button>
-          <button onClick={() => openTerminal(9433, 2)}> Apache Spark </button>
-          <button onClick={openTableauCloud}> Tableau </button>
-          <button onClick={() => handleOnClick(9000)}> SonarQube & SonarScanner </button>
-          <button onClick={() => handleOnClick(9432)}> TensorFlow </button>
-          <button onClick={() => handleOnClick(9090)}> Markdown </button>
-      </div>
+      <h1> CS1660 Data Science Toolbox </h1>
+      <ul>
+        <li><button onClick={() => handleOnClick(10001)}> RStudio </button></li>
+        <li><button onClick={() => handleOnClick(6080)}> Spyder - deleted </button></li>
+        <li><button onClick={openSasCloud}> IBM SAS </button></li>
+        <li><button onClick={() => openTerminal(10000, 3)}>  Git </button></li>
+        <li><button onClick={() => handleOnClick(10000)}> Jupyter Notebook </button></li>
+        <li><button onClick={() => handleOnClick(6901)}>  Orange* </button></li>
+        <li><button onClick={() => handleOnClick(8443)}> Visual Studio Code IDE </button></li>
+      </ul>
+      <ul>
+        <li><button onClick={() => openTerminal(9433, 1)}> Apache Hadoop </button></li>
+        <li><button onClick={() => openTerminal(9433, 2)}> Apache Spark </button></li>
+        <li><button onClick={openTableauCloud}> Tableau </button></li>
+        <li><button onClick={() => handleOnClick(9000)}> SonarQube & SonarScanner* </button></li>
+        <li><button onClick={() => handleOnClick(9432)}> TensorFlow </button></li>
+        <li><button onClick={() => handleOnClick(9090)}> Markdown </button></li>
+      </ul>
       
     </div>
   );
